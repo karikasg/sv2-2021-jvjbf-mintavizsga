681cd9215640775a870d69e1f133515ff098e0ed
karikasg <karikas.gabor@gmail.com>
2021-12-09 18:19:45 +0100

    unique finder

---
+++ src/main/java/uniquefinder/UniqueFinder.java
@@ -0,0 +1,26 @@
+package uniquefinder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UniqueFinder {
+    public List<Character> getUniqueChars(String text) {
+        List<Character> result = new ArrayList<>();
+
+        if (text == null) {
+            throw new IllegalArgumentException("Text is null.");
+        }
+
+        if (text.length()>0) {
+            result.add(text.charAt(0));
+        }
+
+        for (int i = 1; i < text.length(); i++) {
+            if (!result.contains(text.charAt(i))) {
+                result.add(text.charAt(i));
+            }
+        }
+
+        return result;
+    }
+}
